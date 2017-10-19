909b69cc306fce5ccd95db63d78c3f07c3262e05
Maxim Baz <git@maximbaz.com>
2017-10-19 22:57:26 +0200

    Match hidden files correctly in words mode

---
+++ tests/test_get_words.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from extrakto import get_words
+
+
+class TestGetWords(unittest.TestCase):
+    def test_skip_dot_last_word_in_sentence(self):
+        text = "Hello world. Extrakto is an awesome plugin."
+        words = [
+            "Hello", "world", "Extrakto", "awesome", "plugin"
+        ]
+
+        result = get_words(text, 4)
+        self.assertEquals(words, result)
+
+    def test_match_hidden_files(self):
+        text = "one /home/user/.hidden.txt two .hidden.txt three ./.hidden.txt four ../.hidden.txt"
+        words = [
+            "/home/user/.hidden.txt", ".hidden.txt", "./.hidden.txt",
+            "../.hidden.txt"
+        ]
+
+        result = get_words(text, 6)
+        self.assertEquals(words, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
