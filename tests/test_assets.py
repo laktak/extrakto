d4e661493ff9388208c538e59a43ba808766ae48
Christian Zangl <laktak@cdak.net>
2017-11-18 22:47:19 +0100

    fix asset test for py2

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -4,6 +4,7 @@
 import unittest
 import subprocess
 import os
+import sys
 
 class TestAssets(unittest.TestCase):
 
@@ -11,8 +12,12 @@ class TestAssets(unittest.TestCase):
         script_dir = os.path.dirname(os.path.realpath(__file__))
         tests = ['text1', 'text2', 'unicode']
         for test in tests:
-            subprocess.run("../extrakto.py -w < assets/%s.txt | cmp - ./assets/%s_result.txt" %
-                (test, test), shell=True, check=True, cwd=script_dir)
+            if (sys.version_info < (3, 0)):
+                subprocess.check_call("cd '%s'; ../extrakto.py -w < assets/%s.txt | cmp - ./assets/%s_result.txt" %
+                    (script_dir, test, test), shell=True)
+            else:
+                subprocess.run("../extrakto.py -w < assets/%s.txt | cmp - ./assets/%s_result.txt" %
+                    (test, test), shell=True, check=True, cwd=script_dir)
 
 if __name__ == '__main__':
     unittest.main()
