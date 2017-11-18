f8bd69cf557a8464ea0f930ca0fdb1715b85e386
Christian Zangl <laktak@cdak.net>
2017-11-18 22:39:57 +0100

    fix asset test

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -12,7 +12,7 @@ class TestAssets(unittest.TestCase):
         tests = ['text1', 'text2', 'unicode']
         for test in tests:
             subprocess.run("../extrakto.py -w < assets/%s.txt | cmp - ./assets/%s_result.txt" %
-                (test, test), shell=True, check=True)#, cwd=script_dir)
+                (test, test), shell=True, check=True, cwd=script_dir)
 
 if __name__ == '__main__':
     unittest.main()
