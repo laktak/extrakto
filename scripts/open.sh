c1e5cc5082c4f59c6667446194d1589dffb10807
Marc Deop <marc@marcdeop.com>
2024-05-09 18:39:34 +0200

    feat: import functionality from #118 (#119)

--- scripts/open.sh
+++ scripts/open.sh
@@ -19,6 +19,7 @@ if [ "$split_direction" = "a" ]; then
 	fi
 fi
 
+extrakto_inital_mode="$2"
 if [ "$split_direction" = "p" ]; then
 	popup_size=$(get_option "@extrakto_popup_size" "90%")
 	popup_width=$(echo $popup_size | cut -d',' -f1)
@@ -35,11 +36,15 @@ if [ "$split_direction" = "p" ]; then
 			-h "${popup_height:-${popup_width}}" \
 			-x "${popup_x}" \
 			-y "${popup_y:-$popup_x}" \
+			-e extrakto_inital_mode="${extrakto_inital_mode}" \
 			-E "${extrakto} ${pane_id} popup"
 		rc=$?
 	done
 	exit $rc
 else
 	split_size=$(get_option "@extrakto_split_size" 7)
-	tmux split-window -${split_direction} -l ${split_size} "tmux setw remain-on-exit off; ${extrakto} ${pane_id} split"
+	tmux split-window \
+		-${split_direction} \
+		-e extrakto_inital_mode="${extrakto_inital_mode}" \
+		-l ${split_size} "tmux setw remain-on-exit off; ${extrakto} ${pane_id} split"
 fi
